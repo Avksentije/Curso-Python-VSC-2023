7df3a386c8d37c436a1e11b3e76a59a7975ff22f
Avksentije <a.klozevits@gmail.com>
2023-01-09 12:11:14 -0600

    Initial commit

--- Dia1.py
+++ Dia1.py
@@ -1,2 +1,21 @@
 #Hola mundo
-print("Hola, mundo")
+print("Hola, Python")
+print("Soy Tania")
+
+#Consultar tipo de dato
+print(type("Hola, mundo!")) #Tipo 'str'
+print(type(5)) #Tipo 'int'
+print(type(5.5)) #Tipo 'float'
+print(type(True)) #Tipo 'boolean'
+print(type(3 + 1j)) #Numeros complejos 'complex'
+print(type([1, 2, 3])) #Tipo 'lista'
+
+print(2 + 5) #Adicion
+print(2 - 5) #Sustraccion
+print(2 * 5) #Producto o multiplicacion
+print(2 / 5) #Division
+print(2 ** 5) #Exponencial
+print (2 % 5) #Modulo
+print(2 // 5) #Floor division operator
+
+
