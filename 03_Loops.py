02fad4962e18656a274bdcca2749fd67ad56f110
Avksentije <a.klozevits@gmail.com>
2023-02-23 12:02:25 -0600

    Initial commit

--- 03_Loops.py
+++ 03_Loops.py
@@ -32,3 +32,42 @@ while my_condition < 20:
         break
 else:
     print(my_condition)
+
+
+#For: la función for está ligada a un conjunto de valores
+
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+for element in my_list:
+    print(element)
+
+
+tania = ["plantas", "pintura", "agricultura"] #lista
+amurita = ("filosofia", "musica", "pensamiento") #tupla
+playlist_album = {"Agua", "Bonito", "Me gusta como eres"} #set
+person_info = { #diccionario
+   'firstname':'Amurita',
+   'lastname':'Klozevits',
+   'country':'Finland',
+   'city':'Helsinki',
+   'age':35,
+   'is_married': False
+   }
+
+for element in tania:
+    print(element)
+
+for element in amurita:
+    print(element)
+
+for element in playlist_album:
+    print(element)
+
+for element in person_info: # en un diccionario, imprime solo los campos que integran al diccionario
+    print(element)
+
+for element in list(person_info.values()): #al diccionario lo debemos transformar en lista y obtener los valores para que los muestre
+    print(element) 
+
+
