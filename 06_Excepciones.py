96033abfc9ad704feebc8abdb43fca2cb45dd851
Avksentije <a.klozevits@gmail.com>
2023-02-24 20:13:55 -0600

    Initial commit

--- 06_Excepciones.py
+++ 06_Excepciones.py
@@ -2,10 +2,35 @@
 
 numberOne = 5
 numberTwo = 1
-numberTwo = "1"
+numberTwo = "2"
 
 try: 
     print(numberOne + numberTwo)
     print("No se ha producido un error")
 except:
     print("Se ha producido un error")
+else:
+    #Se ejecuta si no se produce una excepción
+    print("La ejecución continúa correctamente")
+finally: #Opcional
+    #Se ejecuta siempre
+    print("La ejecución continúa")
+
+
+
+#Excepciones por tipo de dato
+
+try: 
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except TypeError:
+    print("Se ha producido un error")
+
+
+try: 
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError:
+    print("Se ha producido un error de tipo ValueError")
+except TypeError:
+    print("Se ha producido un error de tipo TypeError")
