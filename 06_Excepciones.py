b08365c4a5df883ede86e0ee7688c18db98d4af2
Avksentije <a.klozevits@gmail.com>
2023-02-24 20:26:00 -0600

    Initial commit

--- 06_Excepciones.py
+++ 06_Excepciones.py
@@ -34,3 +34,21 @@ except ValueError:
     print("Se ha producido un error de tipo ValueError")
 except TypeError:
     print("Se ha producido un error de tipo TypeError")
+
+
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError as yo_defino_el_nombre_de_esta_variable_que_almacena_el_tipo_de_error:
+    print(yo_defino_el_nombre_de_esta_variable_que_almacena_el_tipo_de_error)
+except Exception as esta_otra_de_aca_también:
+    print(esta_otra_de_aca_también)
+
+#pero también lo puedo escribir así, para que sea más estándar jaja
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError as error:
+    print(error)
+except Exception as exception:
+    print(exception)
